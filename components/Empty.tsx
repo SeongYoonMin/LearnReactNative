1b68f1068587c51b73e2ceeab25a90ad95880b47
SeongYoon <namo4862@gmail.com>
2025-02-06 00:40:01 +0900

    fix: Empty Components Styles

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+
+const styles = StyleSheet.create({
+  emptyView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default function Empty() {
   return (
-    <View>
+    <View style={styles.emptyView}>
       <Text>할일이 없습니다.</Text>
     </View>
   );
