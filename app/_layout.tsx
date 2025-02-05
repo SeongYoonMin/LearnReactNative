d36139478ff6e2ee56ab0de010d4c37a8f390526
SeongYoon <namo4862@gmail.com>
2025-02-06 00:39:37 +0900

    fix: Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import DateHeader from "@/components/DateHeader";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <SafeAreaView>
-      <DateHeader />
-      <Stack />
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.select({ ios: "padding"})}
+      >
+        <DateHeader />
+        <Slot />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
